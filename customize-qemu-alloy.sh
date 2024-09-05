cb70d8da18610aad8e4511d08bed61e976aea06c
freitasgst <freitasgst@gmail.com>
2024-09-05 12:44:09 -0300

    use of curl instead of wget

--- customize-qemu-alloy.sh
+++ customize-qemu-alloy.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 # Add Grafana's official GPG key
-mkdir -p /etc/apt/keyrings/
-wget -q -O - https://apt.grafana.com/gpg-full.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+curl -fsSL https://apt.grafana.com/gpg.key -o /etc/apt/keyrings/grafana.gpg | gpg --dearmor
 
 # Add Grafana's official repository to Apt sources
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+cat > /etc/apt/sources.list.d/grafana.list <<-EOF
+    deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com $(lsb_release -cs) stable main
+EOF
 
 # Install packages
 apt-get --yes update
