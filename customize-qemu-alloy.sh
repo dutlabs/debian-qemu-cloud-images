e1e1771564de94f771c28d5925ca8c6fba8ce84b
freitasgst <freitasgst@gmail.com>
2024-09-05 11:52:05 -0300

    fix: test with the other link that grafana offers for the GPG key

--- customize-qemu-alloy.sh
+++ customize-qemu-alloy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Add Grafana's official GPG key
-wget -q -O - https://apt.grafana.com/gpg.key 
+wget -q -O - https://rpm.grafana.com/gpg-full.key 
 
 # Add Grafana's official repository to Apt sources
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
