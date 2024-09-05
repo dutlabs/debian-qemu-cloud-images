75beac59f3525c3a5708d52b7f436309ac67026a
freitasgst <freitasgst@gmail.com>
2024-09-05 12:08:18 -0300

    fix: add grafana's official repository to apt srcs command chore: add htop

--- customize-qemu-alloy.sh
+++ customize-qemu-alloy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # Add Grafana's official GPG key
-wget -q -O - https://rpm.grafana.com/gpg-full.key 
+wget -q -O - https://apt.grafana.com/gpg-full.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Grafana's official repository to Apt sources
-echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+cat > /etc/apt/sources.list.d/grafana.list <<-EOF
+    deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com $(lsb_release -cs) stable main
+EOF
 
 # Install packages
 apt-get --yes update
-apt-get --yes install qemu-guest-agent alloy
+apt-get --yes install qemu-guest-agent htop alloy
