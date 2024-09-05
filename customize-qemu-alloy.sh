e4ee620d7d4020305e2ec921b6da26b290b7df31
freitasgst <freitasgst@gmail.com>
2024-09-05 15:32:34 -0300

    add gpg package and command

--- customize-qemu-alloy.sh
+++ customize-qemu-alloy.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Install required packages
+apt-get --yes update
+apt-get --yes install gpg
+
 # Add Grafana's official GPG key
-curl -fsSL https://apt.grafana.com/gpg-full.key -o /etc/apt/keyrings/grafana.gpg
+curl -fsSL https://apt.grafana.com/gpg-full.key -o /etc/apt/keyrings/grafana.gpg | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Grafana's official repository to Apt sources
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com/ stable main" | tee -i /etc/apt/sources.list.d/grafana.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com/ stable main" | tee /etc/apt/sources.list.d/grafana.list
 
 # Install packages
-apt-get --yes update
 apt-get --yes install htop qemu-guest-agent alloy
