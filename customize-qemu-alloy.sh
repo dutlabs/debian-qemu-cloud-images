76965a689729da8f827f8898938128e84048c9ae
freitasgst <freitasgst@gmail.com>
2024-09-05 15:56:21 -0300

    change commands to be more similar to the grafana documentation

--- customize-qemu-alloy.sh
+++ customize-qemu-alloy.sh
@@ -5,10 +5,11 @@ apt-get --yes update
 apt-get --yes install gpg
 
 # Add Grafana's official GPG key
-curl -fsSL https://apt.grafana.com/gpg-full.key -o /etc/apt/keyrings/grafana.gpg | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Grafana's official repository to Apt sources
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com/ stable main" | tee /etc/apt/sources.list.d/grafana.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
 # Install packages
 apt-get --yes update
