9eac1e3d47bbba61cea2abaf867af2bc82043116
freitasgst <freitasgst@gmail.com>
2024-09-05 12:23:04 -0300

    fix: create folder

--- customize-qemu-alloy.sh
+++ customize-qemu-alloy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 # Add Grafana's official GPG key
+mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg-full.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Grafana's official repository to Apt sources
-cat > /etc/apt/sources.list.d/grafana.list <<-EOF
-    deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com $(lsb_release -cs) stable main
-EOF
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
 
 # Install packages
 apt-get --yes update
-apt-get --yes install qemu-guest-agent htop alloy
+apt-get --yes install htop qemu-guest-agent alloy
