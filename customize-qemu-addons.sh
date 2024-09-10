45fe981390b7de24f76784a4d9653eaed05a9ad1
freitasgst <freitasgst@gmail.com>
2024-09-10 11:52:05 -0300

    join scripts

--- customize-qemu-addons.sh
+++ customize-qemu-addons.sh
@@ -5,17 +5,14 @@ apt-get --yes update
 apt-get --yes install gpg
 
 # Add Grafana's official GPG key
-# curl -fsSL https://apt.grafana.com/gpg.key -o-dir /etc/apt/keyrings/ | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
-
-# Add Docker's official GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 
 # Add Grafana's official repository to Apt sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-# cat > /etc/apt/sources.list.d/grafana.list <<-EOF
-#     deb [arch=$(dpkg --print-architecture) signed-by/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main
-# EOF
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 
 # Add Docker's official repository to Apt sources
 cat > /etc/apt/sources.list.d/docker.list <<-EOF
@@ -24,4 +21,4 @@ EOF
 
 # Install packages
 apt-get --yes update
-apt-get --yes install htop qemu-guest-agent alloy docker-ce
+apt-get --yes install qemu-guest-agent htop docker-ce alloy
