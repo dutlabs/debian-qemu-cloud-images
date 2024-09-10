9d12966db0d700c9439f7f1c0f59435d319720fd
freitasgst <freitasgst@gmail.com>
2024-09-10 11:16:00 -0300

    join commands without padronization

--- customize-qemu-addons.sh
+++ customize-qemu-addons.sh
@@ -5,16 +5,17 @@ apt-get --yes update
 apt-get --yes install gpg
 
 # Add Grafana's official GPG key
-curl -fsSL https://apt.grafana.com/gpg.key -o-dir /etc/apt/keyrings/ | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+# curl -fsSL https://apt.grafana.com/gpg.key -o-dir /etc/apt/keyrings/ | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 
 # Add Grafana's official repository to Apt sources
-# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
-cat > /etc/apt/sources.list.d/grafana.list <<-EOF
-    deb [arch=$(dpkg --print-architecture) signed-by/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main
-EOF
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+# cat > /etc/apt/sources.list.d/grafana.list <<-EOF
+#     deb [arch=$(dpkg --print-architecture) signed-by/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main
+# EOF
 
 # Add Docker's official repository to Apt sources
 cat > /etc/apt/sources.list.d/docker.list <<-EOF
