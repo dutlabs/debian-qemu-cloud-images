4db25dafe60211ab66e1889828497e5dfc508598
freitasgst <freitasgst@gmail.com>
2024-09-10 10:47:28 -0300

    fix:specify arguments when adding alloy's official GPG key

--- customize-qemu-addons.sh
+++ customize-qemu-addons.sh
@@ -5,14 +5,15 @@ apt-get --yes update
 apt-get --yes install gpg
 
 # Add Grafana's official GPG key
-curl -fsSL https://apt.grafana.com/gpg.key -o /etc/apt/keyrings/ | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+curl -fsSL https://apt.grafana.com/gpg.key -o-dir /etc/apt/keyrings/ | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 
 # Add Grafana's official repository to Apt sources
-cat > /etc/apt/sources.list.d/grafana.list <<-EOF 
-    deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+cat > /etc/apt/sources.list.d/grafana.list <<-EOF
+    deb [arch=$(dpkg --print-architecture) signed-by/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main
 EOF
 
 # Add Docker's official repository to Apt sources
