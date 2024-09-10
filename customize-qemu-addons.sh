8cd4dcd3ba4b9af53ca913cd08a8cf8c40a6cfb7
freitasgst <freitasgst@gmail.com>
2024-09-10 12:20:40 -0300

    remove install command

--- customize-qemu-addons.sh
+++ customize-qemu-addons.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+# Alloy
 # Install required packages
 apt-get --yes update
 apt-get --yes install gpg
 
 # Add Grafana's official GPG key
-mkdir -p /etc/apt/keyrings/
 wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
 
 # Add Grafana's official repository to Apt sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
 
+# Docker
 # Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 
@@ -19,6 +20,5 @@ cat > /etc/apt/sources.list.d/docker.list <<-EOF
     deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(lsb_release -cs) stable
 EOF
 
-# Install packages
+# Update
 apt-get --yes update
-apt-get --yes install qemu-guest-agent htop docker-ce alloy
