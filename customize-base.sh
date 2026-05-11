d573dda3a9cd741e27644e1ee36073aec98c4611
Everaldo Canuto <everaldo.canuto@gmail.com>
2026-05-11 08:32:23 -0300

    Add base image
    * htop, locale and curl packages
    * Default locale settings en_US.UTF-8

---
+++ customize-base.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install packages
+apt-get --yes install htop locale curl
+
+# Default locale settings
+sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+localectl set-locale LANG=en_US.UTF-8
