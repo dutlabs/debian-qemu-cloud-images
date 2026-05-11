8a4adf8d5944caa9b8434d85a46b9d87b090d133
Everaldo Canuto <everaldo.canuto@gmail.com>
2026-05-11 09:25:34 -0300

    Fix dependencies

--- customize-base.sh
+++ customize-base.sh
@@ -2,10 +2,9 @@
 # deps: qemu
 
 # Install packages
-apt-get --yes update
 apt-get --yes install htop locales curl
 
 # Default locale settings
 sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-localectl set-locale LANG=en_US.UTF-8
+update-locale LANG=en_US.UTF-8
